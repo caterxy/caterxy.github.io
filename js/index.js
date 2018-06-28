f31d30a5c04725af4163ba4aebc87563d5453b5a
Cater xy <caterxy@qq.com>
2018-06-28 10:31:12 +0800

    初始化

---
+++ js/index.js
@@ -0,0 +1,27 @@
+var userAgent=navigator.userAgent,voiceType,
+    voiceSrc=new Array(
+    "我天性不宜交际",
+    "武林外传_1",
+    "武林外传_2",
+    "武林外传_3"
+    );
+var BrowserType=BrowserType();
+if (BrowserType=="Chrome"||BrowserType=="IE") {
+    voiceType="mp3"
+}
+else {
+    voiceType="ogg"
+};
+
+window.onload=function (){
+    document.getElementById('vPlayer').src="./voice/"+voiceSrc[num]+"."+voiceType;
+
+}
+
+
+function play(num) {
+    var vPlayer=document.getElementById('vPlayer');
+    document.getElementById("voiceName").innerHTML=voiceSrc[num];
+    vPlayer.src="./voice/"+voiceSrc[num]+"."+voiceType;
+    vPlayer.play();
+}
